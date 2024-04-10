466c48594c47f489f27dac589112277a70529727
SkyGrel19 <skibav2002@gmail.com>
2024-04-10 19:18:04 +0300

    add x86_64

--- panther_tracker/run_gradle.sh
+++ panther_tracker/run_gradle.sh
@@ -4,6 +4,7 @@ export ANDROID_HOME="/home/skygrel19/Android/Sdk"
 
 cargo ndk -t arm64-v8a -o app/src/main/jniLibs/ --platform 34 build
 cargo ndk -t x86 -o app/src/main/jniLibs/ --platform 34 build
+cargo ndk -t x86_64 -o app/src/main/jniLibs/ --platform 34 build
 ./gradlew build
 
 ./gradlew installDebug
